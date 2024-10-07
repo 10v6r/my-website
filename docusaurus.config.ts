5f819cdd59ed062e843a27e6e6a842bad36eca66
Дмитрий Гоголев <124041971+10v6r@users.noreply.github.com>
2024-10-07 18:18:33 +0500

    qeeq

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://10v6r.github.io/my-website/',
+  url: 'https://10v6r.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/my-website/',
