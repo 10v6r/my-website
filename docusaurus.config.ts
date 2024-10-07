09b2cd2ba0432b7db2f75a7007d1499bb5a2c91f
Дмитрий Гоголев <124041971+10v6r@users.noreply.github.com>
2024-10-07 17:33:06 +0500

    dsf

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://10v6r.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '10v6r.github.io',
+  baseUrl: '/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
